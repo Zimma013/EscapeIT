c84bffd5af6bcc8ab9a5642df5d5a28ffe4a7a72
tomaboro <motek@student.agh.edu.pl>
2017-10-28 21:20:24 +0200

    Quest still in progess

---
+++ app/src/main/java/pl/hackyeah/positivedevs/escapeit/Quests/OpenQuestionQuest.java
@@ -0,0 +1,63 @@
+package pl.hackyeah.positivedevs.escapeit.Quests;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import pl.hackyeah.positivedevs.escapeit.R;
+
+public class OpenQuestionQuest extends AppCompatActivity {
+
+    ImageView questImage;
+    TextView questTitle;
+    TextView questDescription;
+    EditText answer;
+    Button submitButton;
+    Puzzle currPuzzle;
+
+    public void submitAnswer(View v) {
+        String myAnswer = answer.getText().toString();
+
+        if (currPuzzle.checkAnswer(myAnswer)) {
+            Toast.makeText(this, "You are correct", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "You are wrong", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_question_quest);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        questImage = (ImageView) findViewById(R.id.quest_img);
+        questTitle = (TextView) findViewById(R.id.quest_title);
+        questDescription = (TextView) findViewById(R.id.quest_descripton);
+        answer = (EditText) findViewById(R.id.answer);
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        try {
+            currPuzzle = new Puzzle("test.json", this);
+
+            //questImage.setImageBitmap(currPuzzle.getImgPath());
+            questTitle.setText(currPuzzle.getTitle());
+            questDescription.setText(currPuzzle.getDescription());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
