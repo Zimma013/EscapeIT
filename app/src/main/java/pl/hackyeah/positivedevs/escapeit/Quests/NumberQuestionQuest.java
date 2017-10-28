df1437b2d2e58df013bce94effb0e4b81a652461
tomaboro <motek@student.agh.edu.pl>
2017-10-28 20:44:17 +0200

    Quest in progess

---
+++ app/src/main/java/pl/hackyeah/positivedevs/escapeit/Quests/NumberQuestionQuest.java
@@ -0,0 +1,13 @@
+package pl.hackyeah.positivedevs.escapeit.Quests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NumberQuestionQuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_question_quest);
+    }
+}
