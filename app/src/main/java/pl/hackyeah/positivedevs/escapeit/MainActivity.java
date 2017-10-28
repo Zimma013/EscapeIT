d743aaddf4dd16e8c1ab78e4fc737ee524cb3dfc
tomaboro <motek@student.agh.edu.pl>
2017-10-29 01:57:52 +0200

    YEY!

--- app/src/main/java/pl/hackyeah/positivedevs/escapeit/MainActivity.java
+++ app/src/main/java/pl/hackyeah/positivedevs/escapeit/MainActivity.java
@@ -5,20 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.estimote.cloud_plugin.common.EstimoteCloudCredentials;
 import com.estimote.coresdk.common.config.EstimoteSDK;
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
 import com.estimote.coresdk.observation.region.RegionUtils;
 import com.estimote.coresdk.observation.utils.Proximity;
 import com.estimote.coresdk.recognition.packets.EstimoteLocation;
 import com.estimote.coresdk.service.BeaconManager;
+import com.estimote.internal_plugins_api.cloud.proximity.ProximityAttachment;
+import com.estimote.proximity_sdk.proximity.ProximityObserver;
+import com.estimote.proximity_sdk.proximity.ProximityObserverFactory;
+import com.estimote.proximity_sdk.proximity.ProximityRule;
 
 import java.util.List;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function1;
 import pl.hackyeah.positivedevs.escapeit.Quests.CloseQuestionQuest;
 import pl.hackyeah.positivedevs.escapeit.Quests.OpenQuestionQuest;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    ProximityObserver.Handler observationHandler;
     BeaconManager beaconManager;
 
     private boolean beaconNotificationsEnabled = false;
@@ -31,6 +41,51 @@ public class MainActivity extends AppCompatActivity {
 
         activityShown = false;
 
+        EstimoteCloudCredentials cloudCredentials = new EstimoteCloudCredentials("test-8ly", "5eb18314992a67e64e248a2a8c2e49d8");
+        ProximityObserver proximityObserver = new ProximityObserverFactory().create(getApplicationContext(), cloudCredentials);
+
+        ProximityRule rule1 =
+                proximityObserver.ruleBuilder()
+                        .forAttachmentKeyAndValue("venue", "office")
+                        .withOnEnterAction(new Function1<ProximityAttachment, Unit>() {
+                            @Override
+                            public Unit invoke(ProximityAttachment proximityAttachment) {
+                                Log.i("??", "YEY ?");
+                                return null;
+                            }
+                        })
+                        .withOnExitAction(new Function0<Unit>() {
+                            @Override
+                            public Unit invoke() {
+                            /* Do something here */
+                                Log.i("??", "YEY ??");
+                                return null;
+                            }
+                        })
+                        .withOnChangeAction(new Function1<List<? extends ProximityAttachment>, Unit>() {
+                            @Override
+                            public Unit invoke(List<? extends ProximityAttachment> proximityAttachments) {
+                             /* Do something here */
+                                Intent intent = new Intent(getBaseContext(), CloseQuestionQuest.class);
+                                startActivity(intent);
+                                return null;
+                            }
+                        })
+                        .withDesiredMeanTriggerDistance(2.0)
+                        .create();
+
+        observationHandler =
+                proximityObserver.addProximityRules(rule1)
+                        .withBalancedPowerMode()
+                        .withOnErrorAction(new Function1<Throwable, Unit>() {
+                            @Override
+                            public Unit invoke(Throwable throwable) {
+               /* Do something here */
+                                return null;
+                            }
+                        })
+                        .startWithSimpleScanner();
+        /*
         beaconManager = new BeaconManager(getApplicationContext());
 
         EstimoteSDK.initialize(getApplicationContext(), "escapeit-kgn", "d1c44810de672d7ca34417ae707b2e4e");
@@ -68,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Intent intent = new Intent(getBaseContext(), CloseQuestionQuest.class);
-        startActivity(intent);
+        startActivity(intent);*/
     }
 
     @Override
@@ -124,4 +179,10 @@ public class MainActivity extends AppCompatActivity {
         return beaconNotificationsEnabled;
     }
 */
+
+    @Override
+    protected void onDestroy() {
+        observationHandler.stop();
+        super.onDestroy();
+    }
 }
