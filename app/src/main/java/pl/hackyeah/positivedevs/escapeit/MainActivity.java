7f96d8bf96faa56d3fd7cc9b9d0f6ebadc9b36b5
tomaboro <motek@student.agh.edu.pl>
2017-10-28 14:28:21 +0200

    beacons test

--- app/src/main/java/pl/hackyeah/positivedevs/escapeit/MainActivity.java
+++ app/src/main/java/pl/hackyeah/positivedevs/escapeit/MainActivity.java
@@ -1,13 +1,93 @@
 package pl.hackyeah.positivedevs.escapeit;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.estimote.coresdk.common.config.EstimoteSDK;
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.RegionUtils;
+import com.estimote.coresdk.observation.utils.Proximity;
+import com.estimote.coresdk.recognition.packets.EstimoteLocation;
+import com.estimote.coresdk.service.BeaconManager;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    BeaconManager beaconManager;
+
+    private boolean notificationAlreadyShown = false;
+
+    public void showNotification(String title, String message) {
+        if (notificationAlreadyShown) { return; }
+
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+        notificationAlreadyShown = true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        EstimoteSDK.initialize(getApplicationContext(), "escapeit-kgn", "d1c44810de672d7ca34417ae707b2e4e");
+
+        beaconManager.setLocationListener(new BeaconManager.LocationListener() {
+            @Override
+            public void onLocationsFound(List<EstimoteLocation> beacons) {
+                Log.d("LocationListener", "Nearby beacons: " + beacons);
+                
+                String beaconId = "0f0a87151e9e7f7cee18b217073e0104";
+
+                for (EstimoteLocation beacon : beacons) {
+                    if (beacon.id.toString().equals(beaconId)
+                            && RegionUtils.computeProximity(beacon) == Proximity.NEAR) {
+                        showNotification("Hello world", "Looks like you're near a beacon.");
+                    }
+                }
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override public void onServiceReady() {
+
+                beaconManager.startLocationDiscovery();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.disconnect();
     }
 }
